f0d87faed842f5efa74972986651eb92d51ba0f1
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2019-11-22 23:17:18 +0100

    main: Handle router_factory exceptions

--- christmas/__main__.py
+++ christmas/__main__.py
@@ -4,17 +4,27 @@ Christmas main module
 
 from random import random
 from time import sleep
+import sys
 
 from .config import Config
+from .exceptions import ChristmasError
 from .helpers import usage, trap_signals, cleanup
 from .rainbow import disable_leds
 from .router import router_factory
 
 
+def get_router(conf):
+    try:
+        return router_factory(conf)
+    except (FileNotFoundError, PermissionError, ChristmasError) as e:
+        errprint("error: Can not get router type: {}".format(e))
+        sys.exit(2)
+
+
 def main():
     usage()
     conf = Config()
-    router = router_factory(conf)
+    router = get_router(conf)
 
     trap_signals()
     disable_leds()
